d4b78b03e58ce3950c92c9a6179f7fe0b5c11247
alperencode <alperencode@gmail.com>
2026-01-18 15:18:18 +0000

    fix: osc-52 plugin

--- init.lua
+++ init.lua
@@ -975,6 +975,21 @@ require('lazy').setup({
   },
 
   {
+    'ojroques/nvim-osc52',
+    config = function()
+      require('osc52').setup {
+        max_length = 0,
+        silent = false,
+        trim = false,
+      }
+
+      vim.keymap.set('n', '<leader>y', require('osc52').copy_operator, { expr = true })
+      vim.keymap.set('n', '<leader>yy', '<leader>y_', { remap = true })
+      vim.keymap.set('v', '<leader>y', require('osc52').copy_visual)
+    end,
+  },
+
+  {
     'mfussenegger/nvim-dap',
     dependencies = {
       -- UI plugins for better debugging experience
